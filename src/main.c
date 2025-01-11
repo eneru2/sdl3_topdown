4360c4f564e5d46d48eff1ea14dfadc09c53bc56
eneru2 <elementaov@gmail.com>
2025-01-11 14:39:42 +0100

    episode 2

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #define SDL_MAIN_USE_CALLBACKS
-#include <SDL3/SDL_main.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 
 SDL_Window* window;
 SDL_Renderer* renderer;
+SDL_Texture* player_texture;
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
   SDL_DestroyRenderer(renderer);
@@ -27,6 +29,12 @@ void update() {
 void render() {
   SDL_RenderClear(renderer);
   SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+  
+  SDL_FRect sprite_portion = {17,14,15,18};
+  SDL_FRect player_position = {250, 250, 15, 18};
+  SDL_SetTextureScaleMode(player_texture, SDL_SCALEMODE_NEAREST);
+  SDL_RenderTexture(renderer, player_texture, &sprite_portion, &player_position);
+
   SDL_RenderPresent(renderer);
 }
 
@@ -59,6 +67,9 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
     SDL_Log("Error creating renderer: %s", SDL_GetError());
     return SDL_APP_FAILURE;
   }
+
+  const char path[] = "./char_spritesheet.png";
+  player_texture = IMG_LoadTexture(renderer, path);
 
   return SDL_APP_CONTINUE;
 }
