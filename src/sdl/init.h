4719cb5f10c1674bebc0b6b669b9b77a0dd5bb5b
eneru2 <elementaov@gmail.com>
2025-01-12 01:31:38 +0100

    episode 6

---
+++ src/sdl/init.h
@@ -0,0 +1,5 @@
+#pragma once
+#define SDL_MAIN_USE_CALLBACKS
+#include <SDL3/SDL_main.h>
+#include "common.h"
+#include "../player.h"
---
+++ src/sdl/iterate.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "common.h"
+#include "update.h"
+#include "render.h"
